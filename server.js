f9bfa0fd5e168c3ed6aa3d97e909e8828887678e
Tim Mixell <tim.mixell@gmail.com>
2017-10-25 14:53:17 -0400

    removed cheerio dependency (no longer needed); separated team roster from team info call; added support for pagination on calls that return arrays of records

--- server.js
+++ server.js
@@ -5,48 +5,43 @@
  */
 
 var request = require('request');
-var cheerio = require('cheerio');
+
+const domain = 'https://www.extra-life.org/',
+	donationsUrl = domain + 'index.cfm?fuseaction=donorDrive.participantDonations&participantID=',
+	profileUrl = domain + 'index.cfm?fuseaction=donorDrive.participant&participantID=',
+	teamDonationsUrl = domain + 'index.cfm?fuseaction=donorDrive.teamDonations&teamID=',
+	teamProfileUrl = domain + 'index.cfm?fuseaction=donorDrive.team&teamID=',
+	teamRosterUrl = domain + 'index.cfm?fuseaction=donorDrive.teamParticipants&teamID=';
 
 module.exports = {
 	getUserInfo: function (id, callback) {
 		var profileId = id;
 
 		//generate URLs from id
-		var profileUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donordrive.participant&participantID=' + profileId;
-		var jsonUrl = profileUrl + '&format=json';
+		var url = profileUrl + profileId + '&format=json';
 
 		//declare object for return
 		var userInfoJson = {};
 
-		request(jsonUrl, function (error, response) {
+		request(url, function (error, response) {
 			if (!error && response) {
 				userInfoJson = JSON.parse(response.body);
-				userInfoJson.avatarImageURL = 'http:' + userInfoJson.avatarImageURL;
+				userInfoJson.avatarImageURL = 'https:' + userInfoJson.avatarImageURL;
+				userInfoJson.donateURL = domain + 'index.cfm?fuseaction=donate.participant&participantID=' + id;
 
-				request(profileUrl, function (error, response, html) {
-					if (!error && response) {
-						var $ = cheerio.load(html);
-						var name, image, donateURL, team, teamURL;
+				if (userInfoJson.teamID) {
+					module.exports.getTeamInfo(
+						userInfoJson.teamID,
+						function (data) {
+							userInfoJson.teamName = data.name;
+							userInfoJson.teamURL = data.teamURL;
 
-						$('.btn-support-card').filter(function () {
-							donateURL = $(this).attr('href');
-							userInfoJson.donateURL = donateURL;
-						});
-
-						$('.link-team').filter(function () {
-							var data = $(this);
-							team = data.text();
-							teamURL = 'http://www.extra-life.org/' + data.attr('href');
-							userInfoJson.teamName = team;
-							userInfoJson.teamURL = teamURL;
-						});
-
-						callback(userInfoJson);
-					} else {
-						console.log('Error parsing userInfo URL');
-						callback({ status: 500, message: "There was an error trying to make your request" });
-					}
-				});
+							callback(userInfoJson);
+						}
+					);
+				} else {
+					callback(userInfoJson);
+				}
 			} else {
 				console.log('Error parsing userInfo URL');
 				callback({ status: 500, message: "There was an error trying to make your request" });
@@ -54,14 +49,18 @@ module.exports = {
 		});
 	},
 
-	getRecentDonations: function (id, callback) {
-		var userDonationsJson = { recentDonations: [] };
-		var donationsId = id;
-		var donationsUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participantDonations&participantID=' + donationsId + '&format=json';
+	getRecentDonations: function (id, callback, page) {
+		var userDonationsJson = {};
 
-		request(donationsUrl, function (error, response) {
+		var donationsId = id;
+
+		var url = donationsUrl + donationsId + '&format=json&page=' + (page || 1);
+
+		request(url, function (error, response) {
 			if (!error && response) {
-				userDonationsJson = JSON.parse(response.body);
+				userDonationsJson.countDonations = response.headers['x-total-records'] || 0;
+				userDonationsJson.countPages = Math.ceil(userDonationsJson.countDonations / 100);
+				userDonationsJson.recentDonations = JSON.parse(response.body);
 
 				callback(userDonationsJson);
 			} else {
@@ -72,113 +71,72 @@ module.exports = {
 	},
 
 	getTeamInfo: function (id, callback) {
-
 		var teamInfoId = id;
 
-		var teamUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&teamID=' + teamInfoId;
-		var teamJsonURL = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&teamID=' + teamInfoId + '&format=json';
-		var teamRosterUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.teamParticipants&teamID=' + teamInfoId;
+		var url = teamProfileUrl + teamInfoId + '&format=json';
 
 		var teamInfoJson = {};
 
-		request(teamJsonURL, function (error, response) {
-			if(error || !response) {
+		request(url, function (error, response) {
+			if (!error && response) {
+				teamInfoJson = JSON.parse(response.body);
+				teamInfoJson.avatarImageURL = 'http:' + teamInfoJson.avatarImageURL;
+				teamInfoJson.teamURL = domain + 'index.cfm?fuseaction=donorDrive.team&teamID=' + id;
+
+				callback(teamInfoJson);
+			} else {
 				console.log('Error obtaining team info');
 				callback({ status: 500, message: "There was an error trying to make your request" });
 			}
+		});
+	},
 
-			teamInfoJson = JSON.parse(response.body);
-			teamInfoJson.avatarImageURL = 'http:' + teamInfoJson.avatarImageURL;
-			teamInfoJson.teamURL = teamUrl;
+	getTeamDonations: function (id, callback, page) {
+		var teamDonationsJson = {};
 
-			request(teamRosterUrl, function (error, response, html) {
-				if (!error && response) {
-					var $ = cheerio.load(html);
+		var teamId = id;
 
-					//push array to members key for use in following each function
-					teamInfoJson.members = [];
+		var url = teamDonationsUrl + teamId + '&format=json&page=' + (page || 1);
 
-					$('#team tbody tr').each(function (i, elem) {
-						var data = $(this).children('td').children('a');
-						var memberObj = { name: "", isTeamCaptain: false, raised: "", URL: "", pID: "", image: "" };
-						memberObj.name = data.children('span').children('strong.block').text();
-						memberObj.name = memberObj.name.replace(/(\r\n\t|\n|\r|\t)/gm, "").trim();
-						if (memberObj.name.indexOf('Team Captain') > -1) {
-							memberObj.name = memberObj.name.split('Team Captain')[0].trim();
-							memberObj.isTeamCaptain = true;
-						}
-						memberObj.raised = parseFloat(data.children('span').children('.gray').children('small:first-child').children('strong').text().split('$')[1].replace(/,/g, ''));
-						var memberURL = data.attr('href');
-						memberObj.URL = memberURL;
-						memberObj.pID = parseInt(memberURL.split('participantID=')[1]);
-						memberObj.image = 'http:' + data.children('span').children('.member-avatar').attr('src');
-						teamInfoJson.members.push(memberObj);
-					});
+		request(url, function (error, response) {
+			if (!error && response) {
+				teamDonationsJson.countDonations = response.headers['x-total-records'] || 0;
+				teamDonationsJson.countPages = Math.ceil(teamDonationsJson.countDonations / 100);
+				teamDonationsJson.recentDonations = JSON.parse(response.body);
 
-					callback(teamInfoJson);
-				} else {
-					console.log('Error parsing teamInfo URL');
-					callback({ status: 500, message: "There was an error trying to make your request" });
+				callback(teamDonationsJson);
+			} else {
+				console.log('Error parsing teamDonations URL');
+				callback({ status: 500, message: "There was an error trying to make your request" });
+			}
+		});
+	},
+
+	getTeamRoster: function (id, callback, page) {
+		var teamRosterJson = {};
+
+		var teamId = id;
+
+		var url = teamRosterUrl + teamId + '&format=json&page=' + (page || 1);
+
+		request(url, function (error, response) {
+			if (!error && response) {
+				teamRosterJson.countMembers = response.headers['x-total-records'] || 0;
+				teamRosterJson.countPages = Math.ceil(teamRosterJson.countMembers / 100);
+				teamRosterJson.recentMembers = JSON.parse(response.body);
+
+				for(var i = 0; i < teamRosterJson.recentMembers.length; i++) {
+					teamRosterJson.recentMembers[i].avatarImageURL = 'https:' + teamRosterJson.recentMembers[i].avatarImageURL;
+					teamRosterJson.recentMembers[i].profileURL = profileUrl + teamRosterJson.recentMembers[i].participantID;
 				}
-			});
-		})
+
+				callback(teamRosterJson);
+			} else {
+				console.log('Error parsing teamRoster URL');
+				callback({ status: 500, message: "There was an error trying to make your request" });
+			}
+		});
 	}
-
-	// getTeamDonations: function (id, callback) {
-	// 	var teamId = id;
-
-	// 	var teamRosterURL = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.teamParticipants&teamID=' + teamId + '&format=json';
-
-	// 	var donations = [];
-
-	// 	request(teamRosterURL, function (error, response) {
-	// 		var rosterList = JSON.parse(response.body);
-	// 		for (var i = 0, rosterLen = rosterList.length; i < rosterLen; i++) {
-	// 			var userName = rosterList[i].displayName;
-	// 			var donationUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participantDonations&participantID=' + rosterList[i].participantID + '&format=json';
-
-	// 			console.log(i, rosterList.length);
-
-	// 			request(donationUrl, function (error, response) {
-	// 				var userDonations = JSON.parse(response.body);
-	// 				// var j = 0, donateLength = userDonations.length;
-	// 				var donationsCount = 0;
-	// 				for (var j = 0, len = userDonations.length; j < len; j++) {
-	// 					userDonations[j].donatedTo = userName;
-	// 					donations.push(userDonations[j]);
-
-	// 					donationsCount = donations.length;
-
-	// 					//Bit of a janky fix, but since we can have a TON of calls running async here, 
-	// 					//we check to see if the donations length has checked every 50ms.  Once they
-	// 					//have shown to be equal, we send the callback data
-	// 					timeout = setTimeout(function () {
-	// 						if (donationsCount === donations.length) {
-	// 							console.log('they are equal')
-	// 							var sortByDate = function (a, b) {
-	// 								if (a.createdOn < b.createdOn) {
-	// 									return 1
-	// 								}
-	// 								if (a.createdOn > b.createdOn) {
-	// 									return -1
-	// 								}
-	// 								return 0;
-	// 							}
-	// 							clearTimeout
-	// 							return callback(donations.sort(sortByDate));
-							
-	// 						}else{
-	// 							console.log('notequal');
-	// 						}
-	// 					}, 500)
-	// 				}
-	// 			});
-
-	// 		}
-
-
-	// 	})
-	// }
 }
 
 
